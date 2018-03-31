003b48e7a78ec0eee9cc7c21d2edfbde08f4f143
Luisa Pallares <pallares@princeton.edu>
2018-03-31 17:03:18 -0400

    added variable cpus

--- RNAseq_map_readcounts2.sh
+++ RNAseq_map_readcounts2.sh
@@ -2,18 +2,20 @@
 
 #Batch parameters are specified in test...sh script that calls this script
 
-r3=$1
+
+cpus=$1
 inputcount=$2
 refgenome=$3
 annotation=$4
-
+r3=$5
 
 fqfile=`awk -v file=$SLURM_ARRAY_TASK_ID '{if (NR==file) print $0 }' listfiles`
 echo "filename " $fqfile >&2
+echo "# of cpus" $cpus
 
 #echo "Start trimming"
 trimfile=`basename ${fqfile%R*}trim.fq.gz`
-java -jar ~/bin/Trimmomatic-0.32/trimmomatic-0.32.jar SE -threads 5 $fqfile $trimfile ILLUMINACLIP:AdaptersTrim.fasta:1:30:7 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:20
+java -jar ~/bin/Trimmomatic-0.32/trimmomatic-0.32.jar SE -threads $cpus $fqfile $trimfile ILLUMINACLIP:AdaptersTrim.fasta:1:30:7 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:20
 
 echo "Done with trimming"
 mapfile=${trimfile%trim*}map.bam
@@ -21,7 +23,7 @@ summaryfile=${trimfile%trim*}summarymap.txt
 tmpdir=tmp$trimfile
 mkdir $tmpdir
 echo "Starting mapping"
-~/bin/tophat-2.1.1.Linux_x86_64/tophat2 -o $tmpdir $refgenome $trimfile
+~/bin/tophat-2.1.1.Linux_x86_64/tophat2 -p $cpus -o $tmpdir $refgenome $trimfile
 mv $tmpdir/accepted_hits.bam $mapfile
 mv $tmpdir/align_summary.txt $summaryfile
 echo "Done mapping"
@@ -31,7 +33,7 @@ grep 'of input' $summaryfile >&2
 echo "Start dropping multiple mapping reads"
 #filters out multiple mapping reads
 uniqfile=${mapfile%map.bam}uniq.bam
-samtools view -b -q 50 $mapfile -o $uniqfile
+samtools view -b -@ $cpus -q 50 $mapfile -o $uniqfile
 echo "Done dropping multiple mapping reads"
 
 
@@ -52,12 +54,12 @@ countfile=${dedupfile}.genecount
 
 if [ $inputcount == 'dedup' ]
 then
-	~/bin/subread-1.5.1-Linux-x86_64/bin/featureCounts -T 5 -t exon -g gene_id -a $annotation -o $countfile ${dedupfile}.sorted.dedup.bam
+	~/bin/subread-1.5.1-Linux-x86_64/bin/featureCounts -T $cpus -t exon -g gene_id -a $annotation -o $countfile ${dedupfile}.sorted.dedup.bam
 	echo "Done gene count for deduplicated file" $dedupfile
 else
-	 ~/bin/subread-1.5.1-Linux-x86_64/bin/featureCounts -T 5 -t exon -g gene_id -a $annotation -o $countfile $uniqfile
+	 ~/bin/subread-1.5.1-Linux-x86_64/bin/featureCounts -T $cpus  -t exon -g gene_id -a $annotation -o $countfile $uniqfile
 	echo "Done gene count for non-deduplicated file" $uniqfile
 fi
 
-
+date
 
