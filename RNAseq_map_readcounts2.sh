88141d5bf6820b352703dfd1bdc65d28fc74b54d
Luisa Pallares <pallares@princeton.edu>
2018-03-31 18:07:12 -0400

    new paths, delete some interm. files, fix dedup part

--- RNAseq_map_readcounts2.sh
+++ RNAseq_map_readcounts2.sh
@@ -1,7 +1,6 @@
 #/bin/bash
 
-#Batch parameters are specified in test...sh script that calls this script
-
+#Batch parameters are specified in RNAseq_pipeline_array2 script that calls this script
 
 cpus=$1
 inputcount=$2
@@ -15,19 +14,20 @@ echo "# of cpus" $cpus
 
 #echo "Start trimming"
 trimfile=`basename ${fqfile%R*}trim.fq.gz`
-java -jar ~/bin/Trimmomatic-0.32/trimmomatic-0.32.jar SE -threads $cpus $fqfile $trimfile ILLUMINACLIP:AdaptersTrim.fasta:1:30:7 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:20
+java -jar ~/bin/Trimmomatic-0.32/trimmomatic-0.32.jar SE -threads $cpus $fqfile $trimfile ILLUMINACLIP:/Genomics/grid/users/lamaya/scripts/RNAseq_pipeline/AdaptersTrim.fasta:1:30:7 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:20
 
 echo "Done with trimming"
 mapfile=${trimfile%trim*}map.bam
 summaryfile=${trimfile%trim*}summarymap.txt
 tmpdir=tmp$trimfile
 mkdir $tmpdir
+date
 echo "Starting mapping"
 ~/bin/tophat-2.1.1.Linux_x86_64/tophat2 -p $cpus -o $tmpdir $refgenome $trimfile
 mv $tmpdir/accepted_hits.bam $mapfile
 mv $tmpdir/align_summary.txt $summaryfile
 echo "Done mapping"
-
+date
 grep 'of input' $summaryfile >&2
 
 echo "Start dropping multiple mapping reads"
@@ -37,29 +37,27 @@ samtools view -b -@ $cpus -q 50 $mapfile -o $uniqfile
 echo "Done dropping multiple mapping reads"
 
 
-### if working with TagSeq data, read 3 will be read at the beggining of this script and nudup deduplication will start, otherwise it will not find read 3 and will skip this step writing an error msg in the .error file. 
+if [ -s $uniqfile ]
+        then
+        rm $mapfile
+else
+        echo "removing multiple mapping reads failed - original bam file was kept"
+fi
 
-echo "Start pcr deduplication - nudup"
 # nudup removes (can be just marked if wanted) pcr duplicates useing the UMI from i5 and start pos. It takes for ever to run, couple of hours per sample. If preliminary results wanted, skip this step.
 
 dedupfile=${uniqfile%.bam}
-python ~/bin/nudup/nudup.py -f $r3 -o $dedupfile -s 8 -l 8 --rmdup-only $uniqfile >&2
-echo "Done pcr dedup for" $uniqfile
-
-echo "Start gene count using dedupfile" $dedupfile
-
-countfile=${dedupfile}.genecount
-
-#echo "Skipping nudup, Start gene count using uniqfile" $uniqfile
-
-if [ $inputcount == 'dedup' ]
-then
-	~/bin/subread-1.5.1-Linux-x86_64/bin/featureCounts -T $cpus -t exon -g gene_id -a $annotation -o $countfile ${dedupfile}.sorted.dedup.bam
-	echo "Done gene count for deduplicated file" $dedupfile
+if [ $inputcount == 'dedup' ] && [ -e $r3 ]
+        then
+                echo "Start pcr deduplication - nudup"
+		python ~/bin/nudup/nudup.py -f $r3 -o $dedupfile -s 8 -l 8 --rmdup-only $uniqfile >&2
+		echo "Done pcr dedup for" $uniqfile
+		countfile=${dedupfile}.genecount
+                ~/bin/subread-1.5.1-Linux-x86_64/bin/featureCounts -T $cpus -t exon -g gene_id -a $annotation -o $countfile ${dedupfile}.sorted.dedup.bam
+		echo "Done gene count for deduplicated file" $dedupfile
 else
-	 ~/bin/subread-1.5.1-Linux-x86_64/bin/featureCounts -T $cpus  -t exon -g gene_id -a $annotation -o $countfile $uniqfile
-	echo "Done gene count for non-deduplicated file" $uniqfile
+		countfile=${dedupfile}.genecount
+		 ~/bin/subread-1.5.1-Linux-x86_64/bin/featureCounts -T $cpus -t exon -g gene_id -a $annotation -o $countfile $uniqfile
+		echo "Done gene count for non-deduplicated file" $uniqfile
 fi
-
 date
-
--- RNAseq_pipeline_array2.sh
+++ RNAseq_pipeline_array2.sh
@@ -2,10 +2,10 @@
 #SBATCH --mem=20000
 #SBATCH --time=5:00:00 --qos=1day
 #SBATCH --job-name=RNAseq
-#SBATCH --cpus-per-task=5
+#SBATCH --cpus-per-task=5  ## make sure you modified $cpus accordingly!!!
 #SBATCH --output="%A_%a.out"
 #SBATCH --error="%A_%a.error"
-#SBATCH --array=1-6
+#SBATCH --array=1
 
 
 ##Requires: 
@@ -13,15 +13,19 @@
 # Read 3 (i5)
 # AdaptersTrim.fasta file
 
-## if nudup is gonna be used, set $inputcount to 'dedup', otherwilse set it to ''
-## it requires the script "map_readcounts.sh" located in ~/varios/scripts/map_readcounts.sh
+## DESCRIPTION ##
+# This master scripts loads "RNAseq_map_readcounts2.sh". That script trims the raw fq files, maps using TopHat, removes multiple mapping reads, deduplicates bam files using either Picard or Nudup, and performs gene count using Feature Counts. 
+# After all samples are processed, use the script "RNAse_parsing_output.sh" to generate general stats and a single gene count file for all samples.
 
+## If previously run and failed, make sure there are no files left from the last run, tmp dirs, listfiles, etc. 
 
 date
 set -e 
 # set up the number of cpus to match --cpus-per-task, this value will be use for tophat, samtools, and featurecounts
 cpus=5
-inputcount='dedup'
+## if nudup is gonna be used, set $inputcount to 'dedup', otherwilse set it to 'uniq' and make sure to load read3
+inputcount='uniq'
+r3=*Read_3_Index_Read_passed_filter.fastq*
 
 #refgenome=~/genomes/hsapiens/genome
 #annotation=~/genomes/hsapiens/Homo_sapiens.GRCh38.86.chr.protcoding.gtf
@@ -29,49 +33,7 @@ inputcount='dedup'
 refgenome=~/genomes/dmel_genome/dmel-all-chromosome-r6.14
 annotation=~/genomes/dmel_genome/dmel-all-r6.14.gtf
 
-r3=*Read_3_Index_Read_passed_filter.fastq*
-echo "Read3 read"
-
-##allows one mismatch in the barcodes - This is being run in a differnet script
-#~/bin/fastq-multx/fastq-multx -m 1 -B BarcodeDemultiplex $r2 $r1 -o n/a -o %.R1.fq.gz >&2 
-
-#echo "Demultiplexing done"
-
-
-#for file in *R1.fq.gz
-#do
-#echo $file >> listfiles
-#done
-
-source ~/varios/scripts/RNAseq_map_readcounts2.sh ${cpus} ${inputcount} ${refgenome} ${annotation} ${r3}
-
-#echo "Start merging the counts of all samples"
-
-#for file in *genecount; do cut -f7 $file > tmp$file; cut -f1,2,5,6 $file > names; done 
-#cut -f1 names > a; cut -f2 names | sed 's/;.*//g' > b; cut -f3 names | sed 's/;.*//g' > c ; cut -f4 names > d ; paste a b c d tmp*genecount | sed '1d' > readcountsallsamples.txt ; rm tmp*genecount; rm names
-# rm a b c d  #keep these files, sometimes the readcoutnsallsamples.txt is not ok, for whatever reason. 
-#echo "Done merging counts for all samples, see readcountallsamples.txt"
-
-#echo "Getting read parameters total_reads, kept, dropped, mapped, useful_reads, assigned_genes"
-#parses the .log from previous steps and generates a file with total reads, dropped during trimming, kept, mapped, and total of useful reads. The number of read that were successfully assigned to genes durng gene count is the last column.
-
-#for file in *error ; do awk '{if (NR==1) print $0}' $file  >> ids.txt; done
-#grep 'filename' *error | awk '{print $2}' > ids.txt
-
-#grep 'Input Reads' *error | awk '{print $3"\t"$6"\t"$9}' | sed -e 's/(//g' -e 's/)//g' > tmp.trimmomatic
-
-#grep 'of input' *error | awk '{print $4}' > tmp.mapping
-
-#grep 'Total reads' *error | awk '{print $5}' > tmp.totalreadsgenecount   ##check that adding the log of nudup doesnt affect this grep###
-
-#grep 'Successfully' *error | awk '{print $6}'  > tmp.assignedreadsgenecount
-
-#paste ids.txt tmp.trimmomatic tmp.mapping tmp.totalreadsgenecount tmp.assignedreadsgenecount  > read.parameters.txt
-#sed -i '1isample\ttotal_reads\tkept\tdropped\tmapped\tuseful_reads\tassigned_genes'  read.parameters.txt
-
-#rm ids.txt tmp.trimmomatic tmp.mapping tmp.totalreadsgenecount tmp.assignedreadsgenecount listfiles 
-
-#rm -r tmp*.gz
+source /Genomics/grid/users/lamaya/scripts/RNAseq_pipeline/RNAseq_map_readcounts2.sh ${cpus} ${inputcount} ${refgenome} ${annotation} ${r3}
 
 echo "Done!"
 date
